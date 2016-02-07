ce94a5104c12f571d2384cc9d1f911dd85d5c27c
Vusko <wagmenws@gmail.com>
2016-02-07 14:16:01 +0200

    homework 1
    home work

---
+++ homework 1 (php)/phpdomashno1.php
@@ -0,0 +1,10 @@
+<?php
+$date = "31.01.2016";
+$time = "23:10";
+echo "Today is 31.01.2016 and the time is 23:10!";
+#zada4a 2
+
+$date = "31.01.2016";
+$time = "19:16";
+
+echo "Today is 31.01.2016 and the time is 19:16!";
---
+++ homework 1 (php)/phpdomashno1.php.php
@@ -0,0 +1,4 @@
+<?php
+$date = "31.01.2016";
+$time = "23:10";
+echo "Today is 31.01.2016 and the time is 23:10!";
